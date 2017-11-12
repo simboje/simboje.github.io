e11a90ecf32da70c9f987f3f11e619291f9add97
Siniša Milojević <milojevics@hotmail.com>
2017-11-12 20:37:26 +0100

    Added bootstrap to page, made header row, two content rows and a footer. Added some demo text and js functions.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,43 @@
 
 var bodyColorToggle=1
+var firstRowColor=1
+var secondRowColor=1
 
-function toggleBackground() {
+function toggleBackgroundColor() {
 	if(bodyColorToggle==1)
 	{
-		document.body.style.backgroundColor = "#5691B9";
+		document.body.style.cssText = "background-color: black !important;";
 		bodyColorToggle=0;
 	}
 	else
 	{
-		document.body.style.backgroundColor = "#bd561f";
+		document.body.style.cssText = "background-color: #bd561f !important;";
 		bodyColorToggle=1;
+	}
+}
+
+function toggleFirstRowColor() {
+	if(firstRowColor==1)
+	{
+		document.getElementById("firstRow").style.backgroundColor = "#bd561f";
+		firstRowColor=0;
+	}
+	else
+	{
+		document.getElementById("firstRow").style.backgroundColor = "black";
+		firstRowColor=1;
+	}
+}
+
+function toggleSecondRowColor() {
+	if(secondRowColor==1)
+	{
+		document.getElementById("secondRow").style.backgroundColor = "#bd561f";
+		secondRowColor=0;
+	}
+	else
+	{
+		document.getElementById("secondRow").style.backgroundColor = "black";
+		secondRowColor=1;
 	}
 }
